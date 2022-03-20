f2bd54eeaadf23a98969e4592b13f86fa64f4306
unknown <ngocquy.k171020@gmail.com>
2022-03-21 01:57:31 +0700

    21/3/2022  1:57AM

---
+++ others/testtime.js
@@ -0,0 +1,8 @@
+// var utcSeconds = 1647760550;
+// var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+// d.setUTCSeconds(utcSeconds);
+
+// console.log(d);
+
+var myDate = new Date(1647760550*1000);
+console.log(myDate.toLocaleString()); // 01/10/2020, 10:35:02
--- src/app/controllers/viewData.js
+++ src/app/controllers/viewData.js
@@ -1,18 +1,21 @@
 const Product = require("../model/product")
-
+const Project = require("../model/project")
 class viewData{
 
     product(req, res){
         Product.find({},(err,productDocs)=>{
             // productDocs = productDocs.toString()
-            productDocs = productDocs.map(productDocs => productDocs.toObject())
+            // productDocs = productDocs.map(productDocs => productDocs.toObject())
             console.log(productDocs);
-            res.render("product/product", {productDocs} )
+            // res.render("product/product", {productDocs} )
+            res.json(productDocs)
         })
     }
     product1(req, res){
-        
-            res.render("cv/myCv" )
+        Project.find({},(err,data)=>{
+            res.json(data)
+        })
+            
         
     }
 
